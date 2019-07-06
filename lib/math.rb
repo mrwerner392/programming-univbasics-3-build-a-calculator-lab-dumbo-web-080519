bc6009324d25734e88eb1deeb00f9aefc47aacb6
Matt Werner <mrwerner392@gmail.com>
2019-07-06 15:12:01 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,26 @@
-def addition(num1, num2)
+# more practice with methods and math operations
+# using implicit return (returns value of last executed code in the method)
 
+def addition(num1, num2)
+  num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
 
 def division(num1, num2)
-
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
 
 def square_root(num)
-
+  Math.sqrt(num) # instance method
 end
